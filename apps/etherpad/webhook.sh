f31e80b2deda9e325597369cb9f2f523f20f8ba3
Faizal Abdul Khader <faizal.khader@redhat.com>
2020-09-04 07:09:50 -0500

    Update 14

--- apps/etherpad/webhook.sh
+++ apps/etherpad/webhook.sh
@@ -1,2 +1,2 @@
-oc create route passthrough --service=multicluster-operators-subscription --port=8443 -n acm
+oc create route passthrough --service=multicluster-operators-subscription --port=8443 -n open-cluster-management
 oc annotate channel.apps.open-cluster-management.io etherpad-app-latest apps.open-cluster-management.io/webhook-enabled="true" -n etherpad apps.open-cluster-management.io/webhook-secret=rhacm-github-webhook-secret
